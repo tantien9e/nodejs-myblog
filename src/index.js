f53b0cee1413f0e17d34f050ef91af3bdc8139ea
Nguyen Tan Tien <nguyentantien@Tiens-MacBook-Pro.local>
2023-05-01 10:17:08 +0700

    testing husky2

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.set('view engine', 'hbs')
 
 //The code below change the root directory to ./resources/views (the place where the home.handlebars is located)
 app.set('views', 
-  path.resolve(__dirname, './resources/views'))
+    path.resolve(__dirname, './resources/views'))
 
 route(app)
 
